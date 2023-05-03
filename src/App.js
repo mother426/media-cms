baac0f74f25b9b58d2e5222002cf101521961479
mother426 <johnkimmsterr@gmail.com>
2023-05-03 13:59:47 -0400

    boilerplate work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import UserList from "./components/UserList";
+
 function App() {
-    return <div>app</div>
+    return <div className="container mx-auto"><UserList/></div>
 }
 
 export default App;
