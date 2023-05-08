5be74028219ac587b7c5c248ada063aba541ec00
mother426 <johnkimmsterr@gmail.com>
2023-05-08 11:28:29 -0400

    loader animations

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,5 +1,27 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "../store";
+import Skeleton from "./skeleton";
+
 function UserList() {
-    return 'userlist'
-}
+    const dispatch = useDispatch();
+    const { data, isLoading, error } = useSelector((state) => {
+        return state.users;
+    });
+
+        useEffect(() => {
+            dispatch(fetchUsers())
+        }, [dispatch]);
+        if(isLoading){
+            return <Skeleton times={6} className={"h-10 w-full"}/>
+        }
+        if(error) {
+            return <div>error...</div>
+        }
+
+    return <div>
+        {data.length}
+    </div>
+};
 
 export default UserList;
