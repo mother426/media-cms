6b8d6f40baed3e3cd42c242dd6072aa972f26075
mother426 <johnkimmsterr@gmail.com>
2023-05-08 14:39:31 -0400

    no quick refresh

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,46 +1,56 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers, createUser } from "../store";
 import Skeleton from "./skeleton";
-// import { Button } from "./Button";
 import Button from "./Button";
 
 function UserList() {
-    const dispatch = useDispatch();
-    const { data, isLoading, error } = useSelector((state) => {
-        return state.users;
-    });
+  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+  const [loadingUsersError, setLoadingUsersError] = useState(null);
 
-        useEffect(() => {
-            dispatch(fetchUsers())
-        }, [dispatch]);
+  const dispatch = useDispatch();
+  const { data } = useSelector((state) => {
+    return state.users;
+  });
 
-        const handleUserAdd = () => {
-            dispatch(createUser())
-        };
+  useEffect(() => {
+    setIsLoadingUsers(true);
+    dispatch(fetchUsers())
+      .unwrap()
+      .catch((error) => setLoadingUsersError(error))
+      .finally(() => setIsLoadingUsers(false));
+  }, [dispatch]);
 
-        if(isLoading){
-            return <Skeleton times={6} className={"h-10 w-full"}/>
-        }
-        if(error) {
-            return <div>error...</div>
-        }
+  const handleUserAdd = () => {
+    dispatch(createUser());
+  };
 
-        const renderedUsers = data.map((user) => {
-            return <div ket={user.id} className="mb-2 border rounded">
-                <div className="flex p-2 justify-between items-center cursor-pointer">
-                    {user.name}
-                </div>
-            </div>
-        })
+  if (isLoadingUsers) {
+    return <Skeleton times={6} className={"h-10 w-full"} />;
+  }
+  if (loadingUsersError) {
+    return <div>error...</div>;
+  }
 
-    return <div>
-        <div className="flex flex-row justify-between m-3">
-        <h1 className="m-2 text-xl">users</h1>
-        <Button onClick={handleUserAdd}>+ Add User</ Button>
+  const renderedUsers = data.map((user) => {
+    return (
+      <div ket={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
         </div>
-        {renderedUsers}
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="flex flex-row justify-between m-3">
+        <h1 className="m-2 text-xl">users</h1>
+        <Button onClick={handleUserAdd}>+ Add User</Button>
+      </div>
+      {renderedUsers}
     </div>
-};
+  );
+}
 
 export default UserList;
