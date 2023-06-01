faa01a0e6ef4084b094b077c2be41fb098902112
mother426 <johnkimmsterr@gmail.com>
2023-06-01 17:57:37 -0400

    show, add remove photos to albums finished app

--- src/components/PhotosListItem.js
+++ src/components/PhotosListItem.js
@@ -1,5 +1,20 @@
-function PhotosListItem() {
-    return <div>phots list item</div>
+import { GoTrashcan } from "react-icons/go";
+import { useRemovePhotoMutation } from "../store";
+
+function PhotosListItem({ photo }) {
+  const [removePhoto] = useRemovePhotoMutation();
+
+  const handleRemovePhoto = () => {
+    removePhoto(photo);
+  };
+  return (
+    <div className="relative m-2 cursor-pointer" onClick={handleRemovePhoto}>
+      <img className="h-20 w-20" src={photo.url} alt="random" />
+      <div className="absolute inset-0 flex items-center justify-center hover:bg-gray-200 opacity-0 hover:opacity-80">
+        <GoTrashcan className="text-3xl" />
+      </div>
+    </div>
+  );
 }
 
 export default PhotosListItem;
--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -4,7 +4,7 @@ import { faker } from "@faker-js/faker";
 const albumsApi = createApi({
   reducerPath: "albums",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3005",
+    baseUrl: "http://localhost:3005"
   }),
   endpoints(builder) {
     return {
